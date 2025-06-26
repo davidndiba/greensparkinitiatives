597990575fa3bbee758072f15a90721bb373eeb1
David Ndiba <davidndiba20@gmail.com>
2025-06-26 18:00:30 +0300

    resolved mobile responsiveness

--- clientsiteGSI/script.js
+++ clientsiteGSI/script.js
@@ -1,4 +1,14 @@
 // script.js
+// script.js
+const toggle = document.querySelector('.nav-toggle');
+const menu   = document.querySelector('.nav-menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('nav-menu--open');
+});
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const btn  = document.querySelector('.nav-toggle');
   const menu = document.querySelector('.nav-menu');
